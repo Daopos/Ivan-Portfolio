ee209c14d8c49934ffdac82d17a789a8077ceab6
Ivan <ivan.munoz.50309277@gmail.com>
2025-06-30 19:41:18 +0800

    Adding Navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./Navbar.css";
+import logo from "./assets/logo.png";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <img src={logo} alt="" />
+        </div>
+
+        <div className="burger" onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+
+        <ul className={`nav-links ${isOpen ? "active" : ""}`}>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
